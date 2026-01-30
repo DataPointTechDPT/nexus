5e65076dd4ed3e019504bbf80fb642a083a51172
Luiggi Alarcon <luiggis.ao@hotmail.com>
2026-01-30 15:25:56 -0500

    feat: add initial Terraform configuration for AWS resources

---
+++ terraform/database.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "nexus-rds-sg"
+  description = "Allow traffic from NestJS app"
+}
+
+resource "aws_security_group_rule" "rds_ingress_internal" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = data.aws_security_group.eb_sg.id
+}
+
+resource "aws_security_group_rule" "rds_ingress_external" {
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  security_group_id = aws_security_group.rds_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "rds_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.rds_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# 2. RDS PostgreSQL Instance
+resource "aws_db_instance" "postgres" {
+  allocated_storage          = 20
+  engine                     = "postgres"
+  engine_version             = "16"
+  instance_class             = "db.t4g.micro"
+  db_name                    = "nexus_db"
+  username                   = "nexus_admin"
+  password                   = var.db_password
+  parameter_group_name       = "default.postgres16"
+  skip_final_snapshot        = true
+  publicly_accessible        = true
+  vpc_security_group_ids     = [aws_security_group.rds_sg.id]
+  auto_minor_version_upgrade = true
+}
+
+data "aws_security_group" "eb_sg" {
+  filter {
+    name   = "tag:elasticbeanstalk:environment-id"
+    values = [aws_elastic_beanstalk_environment.env.id]
+  }
+
+  filter {
+    name   = "group-name"
+    values = ["*AWSEBSecurityGroup*"]
+  }
+}
